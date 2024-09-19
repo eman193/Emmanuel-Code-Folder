cfc4fe522e74c3158e6855c3ce7aa5dabac5de36
eman193 <emmanuelmok28@gmail.com>
2024-09-19 09:08:02 -0600

    chicken wings A6

--- 1-lessons and demos/ChickenWings/src/App.java
+++ 1-lessons and demos/ChickenWings/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         double chicken= 4.75;
         double total= (chicken / 0.75);
-        System.out.println((int)total + "chicken wings!");
+        System.out.println("You can buy " + (int)total + " chicken wings!");
 
     }
 }
