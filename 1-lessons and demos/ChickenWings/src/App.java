e685341bfbc8f1b0c6f2ef4a6c67aa2e49f72393
eman193 <emmanuelmok28@gmail.com>
2024-09-19 09:22:08 -0600

    chickenwing UPDATED

--- 1-lessons and demos/ChickenWings/src/App.java
+++ 1-lessons and demos/ChickenWings/src/App.java
@@ -1,3 +1,9 @@
+//Chicken Wings
+//Emmanuel Mok
+//Sept. 18th, 2024
+//click run and the program will tell you how many chickens you can buy
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         double chicken= 4.75;
