f3c69c75efe4b275079c1a1400e55c491ad7ca39
eman193 <emmanuelmok28@gmail.com>
2024-10-01 09:08:58 -0600

    lesson 8

---
+++ 1-lessons and demos/Lesson8/src/Turkey.java
@@ -0,0 +1,10 @@
+public class Turkey {
+    public void speak() {
+        System.out.println("gobble-goibble!");
+    }   
+    public void sayName() {
+        System.out.println("big birg");
+        speak();
+
+    }
+}
---
+++ 1-lessons and demos/Lesson8/src/Turkeyrunner.java
@@ -0,0 +1,11 @@
+public class Turkeyrunner {
+    public static void main(String[] args) throws Exception {
+        Turkey bird = new Turkey();
+
+        bird.speak();
+        bird.speak();
+        bird.speak();
+        bird.sayName();
+        
+    }
+}
