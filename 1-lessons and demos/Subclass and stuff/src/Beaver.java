23fa01dc777e6834af3d6af51f88aaeba79a6515
eman193 <emmanuelmok28@gmail.com>
2024-10-01 09:08:35 -0600

    BEAVER A8b

---
+++ 1-lessons and demos/Subclass and stuff/src/Beaver.java
@@ -0,0 +1,10 @@
+public class Beaver {
+    public void speak() {
+        System.out.println("Building a dam!");
+    }
+    public void sayDam() {
+        System.out.println("beaver beaver");
+        speak();
+    }
+
+}
---
+++ 1-lessons and demos/Subclass and stuff/src/Beaverdam.java
@@ -0,0 +1,17 @@
+//Subclass and stuff
+//Emmanuel Mok
+//October 1, 2024
+//beaver builds a dam
+
+
+
+public class Beaverdam {
+    public static void main(String[] arg) throws Exception {
+        Beaver dam = new Beaver();
+        dam.speak();
+        dam.sayDam();
+
+
+    }
+
+}
