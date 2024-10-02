dddf02e8d2959584ab130489b41bf20a333ef252
eman193 <emmanuelmok28@gmail.com>
2024-10-02 09:29:09 -0600

    lesson 9

---
+++ 1-lessons and demos/Lesson9 conditkions/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean isCold = false;
+
+        if (isCold==true) {
+            System.out.println("wear a coat");
+            System.out.println("wear gloves too");
+        }
+        System.out.println("bye");
+    }
+}
