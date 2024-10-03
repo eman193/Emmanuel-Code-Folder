abb72668c8124b80baf920f501d3d7b700827e44
eman193 <emmanuelmok28@gmail.com>
2024-10-03 09:48:16 -0600

    BIGORSMALL A9

---
+++ 1-lessons and demos/BigOrSmall/src/BigOrSmall.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class BigOrSmall {
+    public void biggiesmalls() {
+        Scanner keyboard = new Scanner(System.in);
+        int num1,num2;
+        System.out.println("Enter first integer: ");
+        num1 = keyboard.nextInt();
+        System.out.println("Enter second integer: ");
+        num2 = keyboard.nextInt();
+        if (num1 == num2)
+        System.out.println("Integers are equal");
+        else if (num1 > num2)
+        System.out.println("First integer is bigger");
+        else if (num1 < num2)
+        System.out.println("First Integer is smaller");
+
+        }
+}
---
+++ 1-lessons and demos/BigOrSmall/src/BigOrSmallRunner.java
@@ -0,0 +1,8 @@
+public class BigOrSmallRunner {
+    public static void main(String[] args) throws Exception {
+        BigOrSmall num = new BigOrSmall();
+        num.biggiesmalls();
+        
+    }
+
+}
