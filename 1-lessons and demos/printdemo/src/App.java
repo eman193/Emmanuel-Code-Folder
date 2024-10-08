80eeec6823be582667945ffc2c2ca715a08ecdde
eman193 <emmanuelmok28@gmail.com>
2024-10-08 09:15:26 -0600

    lessons 10

---
+++ 1-lessons and demos/printdemo/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SubMethods fn = new SubMethods();
+        fn.calc(a:7,b:13);
+        fn.output();
+    }
+}
---
+++ 1-lessons and demos/printdemo/src/SubMethods.java
@@ -0,0 +1,20 @@
+public class SubMethods {
+    private int ans = 0;
+
+    public void calc(int a, int b) {
+        ans = (a * b);
+    }
+    public void output() {
+        System.out.println(ans);
+        System.out.printf("%.2f",ans);
+
+    
+        /*
+         * %f - real/decimal num
+         * %d - int value
+         * %c - char value
+         * %s - strin value
+         */
+
+    }
+}
