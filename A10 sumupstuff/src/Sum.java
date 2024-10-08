ef12883887a3b814dc99db3c9c20e9a34cdff222
eman193 <emmanuelmok28@gmail.com>
2024-10-08 09:15:44 -0600

    A10 SUMS

---
+++ A10 sumupstuff/src/Sum.java
@@ -0,0 +1,16 @@
+public class Sum {
+	private double one, two, sum;
+
+	public void setNums(double num1, double num2)
+	{one = num1;
+    two = num2;
+	}
+
+	public void sum()
+	{sum = one + two;
+	}
+
+	public void print( )
+    {System.out.println(sum);
+    }
+}
---
+++ A10 sumupstuff/src/SumRunner.java
@@ -0,0 +1,14 @@
+//A10 sumupstuff
+//Emmanuel Mok
+//October 8th, 2024
+//enter double value into the perams, run to get sum of both values
+
+
+public class SumRunner {
+    public static void main(String[] args) {
+        Sum test = new Sum();
+        test.setNums(2,6);
+		test.sum();
+		test.print();
+    }
+}
