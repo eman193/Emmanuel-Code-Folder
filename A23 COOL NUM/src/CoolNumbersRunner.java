50372895cfb0a1a0a3f481e1c5260ab97242f200
eman193 <emmanuelmok28@gmail.com>
2024-11-04 09:31:40 -0600

    A23

---
+++ A23 COOL NUM/src/CoolNumbersRunner.java
@@ -0,0 +1,9 @@
+
+
+public class CoolNumbersRunner
+{
+	public static void main( String[] args )
+	{
+		System.out.println( CoolNumbers.countCoolNumbers(250) + " cool numbers between 6 - " + 250);
+	}
+}
